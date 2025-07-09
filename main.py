c41332bbd30411012f871d3540b873ae5882a0d3
DmitriiMurovtsev <mdv12672@gmail.com>
2025-07-09 10:39:20 +0300

    Добавлено новое асинхронное определение функции get_answer для отправки запросов к векторной БД и получения ответов, а также улучшена читаемость кода в функции read_root.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI, Request
 from gigachat import GigaChat
 from dotenv import load_dotenv
+from typing import Dict
 
 load_dotenv()
 GIGA_API_KEY = os.getenv("GIGA_API_KEY")
@@ -18,7 +19,7 @@ async def read_root(request: Request):
         
         with GigaChat(credentials=GIGA_API_KEY, model="GigaChat", verify_ssl_certs=False) as chat:
             response = chat.chat(prompt)
-            answer =    response.choices[0].message.content
+            answer = response.choices[0].message.content
             return {"answer": answer}
     except Exception as e:
         return {"error": str(e)}
@@ -34,3 +35,8 @@ async def add_question(request: Request):
         return {"message": "Question added successfully"}
     except Exception as e:
         return {"error": str(e)}
+    
+    
+async def get_answer(question: str) -> Dict[str, str]:
+    """ Отправляет запрос к векторной БД и получает ответ. """
+    pass
